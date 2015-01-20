9fe942773172a898d301b0b6621431fd939fa539
Chinedum Mac <cukejianya@gmail.com>
2015-01-20 01:11:10 -0500

    Add some Array & TryCatch examples and finish study session 4

---
+++ src/TryCatchFinally.java
@@ -0,0 +1,20 @@
+
+public class TryCatchFinally {
+	public static int divide(int x, int y){
+		int retval;
+		
+		try {
+			retval = x/y;
+		} catch (ArithmeticException e) {
+			throw e;
+		} finally {
+			System.out.println("Code has been tried.");
+		}
+		
+		return retval;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(divide(1,0));
+	}
+}
